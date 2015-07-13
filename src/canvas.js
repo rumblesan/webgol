54836498dfa40f5e327a63e4260ea63077732272
Guy John <guy@rumblesan.com>
2015-07-13 10:58:00 +0100

    Improving style of canvas

--- src/canvas.js
+++ src/canvas.js
@@ -11,35 +11,26 @@ Canvas.create = function (canvasEl, columns, rows, cellSize) {
         ctx: canvasEl.getContext("2d")
     };
 
-    Canvas.drawGrid(canvas);
     return canvas;
 
 };
 
-Canvas.drawGrid = function (canvas) {
-    canvas.ctx.fillStyle = "black";
-    var size = canvas.cellSize - 1;
-    var c, r, x, y;
-    for (c = 0; c < canvas.columns; c += 1) {
-        x = c * canvas.cellSize;
-        for (r = 0; r < canvas.rows; r += 1) {
-            y = r * canvas.cellSize;
-            canvas.ctx.fillRect(x, y, size, size);
-        }
-    }
-};
-
 Canvas.drawCell = function (canvas, column, row, value) {
-    var x, y;
+    var x, y, s;
+    s = canvas.cellSize;
+    x = column * s;
+    y = row * s;
     if (value === 0) {
-        canvas.ctx.fillStyle = "black";
-    } else {
         canvas.ctx.fillStyle = "white";
+        canvas.ctx.fillRect(x, y, s, s);
+        canvas.ctx.fillStyle = "black";
+        canvas.ctx.fillRect(x + 1, y + 1, s - 2, s - 2);
+    } else {
+        canvas.ctx.fillStyle = "black";
+        canvas.ctx.fillRect(x, y, s, s);
+        canvas.ctx.fillStyle = "white";
+        canvas.ctx.fillRect(x + 1, y + 1, s - 2, s - 2);
     }
-    x = column * canvas.cellSize;
-    y = row * canvas.cellSize;
-
-    canvas.ctx.fillRect(x, y, canvas.cellSize - 1, canvas.cellSize - 1);
 };
 
 module.exports = Canvas;

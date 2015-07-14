ba1d6f5883b2f9bee4146e683e3a826c0bdb75bb
Guy John <guy@rumblesan.com>
2015-07-14 08:51:43 +0100

    Adding toggle button to start/stop simulation
    ...once it's hooked up

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,12 +1,15 @@
 
 var React = require('react');
 
+var ToggleNavButton = require('./toggleNavButton.jsx');
+
 var NavBar = React.createClass({
     render: function () {
         return (
             <div id='header'>
                 <div id='menu'>
                     <span className='menu-item'>Rumblesan</span>
+                    <ToggleNavButton defValue={true} text='Running'/>
                 </div>
             </div>
         );
